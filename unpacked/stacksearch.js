049468dfd9b71c0dd0a336caeeec1ae8af194897
rlemon <rob.lemon@gmail.com>
2012-01-16 16:10:51 -0500

    page handshakes figured out

--- unpacked/stacksearch.js
+++ unpacked/stacksearch.js
@@ -1,9 +1,18 @@
-var se_site_list = [];
+
+function json_to_array(str) {
+	try {
+		return JSON.parse( str );
+	} catch( e ) {
+		return [];
+	}
+}
 
 function genericOnClick(info) {
 	window.open( se_site_list[ info.menuItemId - 1 ][1] + '/search?q=' + encodeURIComponent( info.selectionText ) );
 }
+
 function updateMenus() {
+	se_site_list = json_to_array(localStorage["ss_site_list"]);
 	for( var i = 0, l = se_site_list.length; i < l; i++ ) {
 		var menu_child = chrome.contextMenus.create( {
 			"title": se_site_list[i][2],
@@ -13,4 +22,5 @@ function updateMenus() {
 	}
 }
 
+var se_site_list;
 updateMenus();
