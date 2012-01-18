17177bda152adfa2d4a52a578cc2f80e1ace2afa
rlemon <rob.lemon@gmail.com>
2012-01-18 16:54:30 -0500

    moving so I can finish later

--- package/stacksearch.js
+++ package/stacksearch.js
@@ -8,21 +8,24 @@ function json_to_array(str) {
 }
 
 function genericOnClick(info) {
-	window.open( se_site_list[ info.menuItemId - 1 ][0] + '/search?q=' + encodeURIComponent( info.selectionText ) );
+	window.open( ss_site_list[ info.menuItemId - 1 ][0] + '/search?q=' + encodeURIComponent( info.selectionText ) );
 }
 
 function updateMenus() {
 	chrome.contextMenus.removeAll(function() {
-		se_site_list = json_to_array(localStorage["ss_site_list"]);
-		for( var i = 0, l = se_site_list.length; i < l; i++ ) {
+		ss_site_list = json_to_array(localStorage['ss_site_list']);
+		for( var i = 0, l = ss_site_list.length; i < l; i++ ) {
 			var menu_child = chrome.contextMenus.create( {
-				"title": se_site_list[i][1],
-				"contexts": ["selection"],
-				"onclick": genericOnClick
+				'title': se_site_list[i][1],
+				'contexts': ['selection'],
+				'onclick': genericOnClick
 			} );
 		}
 	});
 }
 
-var se_site_list;
-updateMenus();
+function run(json) {
+	alert(json.items.length);
+}
+
+var se_site_list, ss_site_list;
